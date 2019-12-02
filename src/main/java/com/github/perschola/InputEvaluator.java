3e548cc457e6995f30a58c8d02af7025db6956a5
perscholas-lhunter <lhunter@perscholas.org>
2019-12-02 11:56:40 -0500

    update

---
+++ src/main/java/com/github/perschola/InputEvaluator.java
@@ -0,0 +1,16 @@
+package com.github.perschola;
+
+public class InputEvaluator {
+    public void run() {
+        // prompt user to input name
+        // get name input from user
+
+        // evaluate name input from user
+            // if name is not "Alice" nor "Bob"
+                // display "Stranger danger!" to console
+            // if name is "Alice"
+                // display "Welcome, Alice!"
+            // if name is "Bob"
+                // display "Welcome, Bob!"
+    }
+}
---
+++ src/main/java/com/github/perschola/MainApplication.java
@@ -0,0 +1,8 @@
+package com.github.perschola;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        InputEvaluator myObject = new InputEvaluator();
+        myObject.run();
+    }
+}
