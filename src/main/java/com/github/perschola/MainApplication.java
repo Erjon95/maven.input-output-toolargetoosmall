5cb608b32250d6d5ddbe8855e5079b708c1f7410
perscholas-lhunter <lhunter@perscholas.org>
2019-12-02 11:56:04 -0500

    removed ignored files

--- src/main/java/com/github/perschola/MainApplication.java
+++
@@ -1,8 +0,0 @@
-package com.github.perschola;
-
-public class MainApplication {
-    public static void main(String[] args) {
-        InputEvaluator myObject = new InputEvaluator();
-        myObject.run();
-    }
-}
